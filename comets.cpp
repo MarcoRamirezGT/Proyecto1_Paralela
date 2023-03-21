a5bae34c884f9152c1eb5a49629d39c35c517913
Estuardo07 <xavihernand7@hotmail.com>
2023-03-20 20:34:49 -0600

    Update comets.cpp

--- comets.cpp
+++ comets.cpp
@@ -3,12 +3,13 @@
 #include <ctime>
 #include <vector>
 #include <SDL2/SDL.h>
+#include <algorithm>
 
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 const int COMET_SIZE = 10;
 const int MIN_SPEED = 1;
-const int MAX_SPEED = 5;
+const int MAX_SPEED = 1;
 const int COLLISION_DISTANCE = COMET_SIZE * 2;
 
 struct Comet {
@@ -18,11 +19,42 @@ struct Comet {
     int vy;
 };
 
+// bool check_collision(const Comet& comet1, const Comet& comet2) {
+//     int dx = comet1.x - comet2.x;
+//     int dy = comet1.y - comet2.y;
+//     int distance_squared = dx * dx + dy * dy;
+//     return distance_squared < COLLISION_DISTANCE * COLLISION_DISTANCE;
+// }
+
 bool check_collision(const Comet& comet1, const Comet& comet2) {
+    SDL_Rect rect1 = {comet1.x, comet1.y, COMET_SIZE, COMET_SIZE};
+    SDL_Rect rect2 = {comet2.x, comet2.y, COMET_SIZE, COMET_SIZE};
+    return SDL_HasIntersection(&rect1, &rect2) == SDL_TRUE;
+}
+
+void handle_collision(Comet& comet1, Comet& comet2) {
     int dx = comet1.x - comet2.x;
     int dy = comet1.y - comet2.y;
     int distance_squared = dx * dx + dy * dy;
-    return distance_squared < COLLISION_DISTANCE * COLLISION_DISTANCE;
+    if (distance_squared < COLLISION_DISTANCE * COLLISION_DISTANCE) {
+        // Invertir velocidades perpendiculares al plano de colisión
+        if (dx != 0) {
+            int vx1_perp = comet1.vy * dx / distance_squared;
+            int vx2_perp = comet2.vy * dx / distance_squared;
+            comet1.vx -= vx1_perp;
+            comet2.vx -= vx2_perp;
+            comet1.vy += vx1_perp;
+            comet2.vy += vx2_perp;
+        }
+        if (dy != 0) {
+            int vy1_perp = comet1.vx * dy / distance_squared;
+            int vy2_perp = comet2.vx * dy / distance_squared;
+            comet1.vy -= vy1_perp;
+            comet2.vy -= vy2_perp;
+            comet1.vx += vy1_perp;
+            comet2.vx += vy2_perp;
+        }
+    }
 }
 
 int main(int argc, char* argv[]) {
@@ -95,16 +127,36 @@ int main(int argc, char* argv[]) {
         }
 
         // Check for collisions and remove collided comets
+        // for (int i = 0; i < comets.size(); ++i) {
+        //     for (int j = i + 1; j < comets.size(); ++j) {
+        //         if (check_collision(comets[i], comets[j])) {
+        //             comets.erase(comets.begin() + j);
+        //             comets.erase(comets.begin() + i);
+        //             --i;
+        //             break;
+        //         }
+        //     }
+        // }
+
+        // Check for collisions and handle collided comets
         for (int i = 0; i < comets.size(); ++i) {
             for (int j = i + 1; j < comets.size(); ++j) {
                 if (check_collision(comets[i], comets[j])) {
-                    comets.erase(comets.begin() + j);
-                    comets.erase(comets.begin() + i);
-                    --i;
-                    break;
+                    handle_collision(comets[i], comets[j]);
                 }
             }
-        }   
+        }
+
+        // Remove comets that have gone off-screen
+        comets.erase(
+            std::remove_if(
+                comets.begin(), comets.end(),
+                [](const Comet& comet) {
+                    return comet.x < 0 || comet.x > SCREEN_WIDTH - COMET_SIZE ||
+                        comet.y < 0 || comet.y > SCREEN_HEIGHT - COMET_SIZE;
+                }),
+            comets.end());
+
 
         // Draw comets
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
